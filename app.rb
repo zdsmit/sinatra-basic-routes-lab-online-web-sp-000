c0e19006bba566f091d3cd4e4a6eea0aa515f86b
Zach  Smith <zdsmith001@gmail.com>
2019-11-12 18:00:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is _"
+  end
+
+  get '/hometown' do
+    "My hometown is _"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is _"
+  end
+
 end
